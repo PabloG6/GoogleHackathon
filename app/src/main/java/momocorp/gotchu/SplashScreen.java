220af8280f6a8b598e97fecf04df1e7cd6870c76
Pablo Grant <Pablo Grant>
2016-10-29 16:33:05 -0400

    Added splash screen

---
+++ app/src/main/java/momocorp/gotchu/SplashScreen.java
@@ -0,0 +1,30 @@
+package momocorp.gotchu;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+    Context mContext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = this;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(mContext, QuestionActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        }, 3000);
+
+
+
+
+    }
+}
